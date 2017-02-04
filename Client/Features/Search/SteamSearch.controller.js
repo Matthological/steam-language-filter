dcf7ed967383c1657edcdbd874d6859539d9c530
Matthias Martineau <matthological@gmail.com>
2017-02-04 10:20:06 -0500

    Work done

--- Client/Features/Search/SteamSearch.controller.js
+++ Client/Features/Search/SteamSearch.controller.js
@@ -1,9 +1,7 @@
 angular.module("app", []).controller('SteamSearch', function($scope, ServerCommunication){
     ServerCommunication.get('language').then(function(res){
-        debugger;
         $scope.languages = res.data;
     }, function(err){
-        debugger;
         console.log(err);
     });
 });
